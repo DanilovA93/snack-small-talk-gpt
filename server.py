a073ae16d3cca037b9b7602ed3bb82003fa308e1
Anton D <41110190+DanilovA93@users.noreply.github.com>
2024-02-27 22:08:35 +0300

    Update server.py

--- server.py
+++ server.py
@@ -21,7 +21,7 @@ class Handler(http.server.SimpleHTTPRequestHandler):
     def do_POST(self):
         content_len = int(self.headers.get('Content-Length'))
         message = json.loads(self.rfile.read(content_len))
-        text = message['text']
+        text = message['prompt']
         new_user_input_ids = tokenizer.encode(text + tokenizer.eos_token, return_tensors='pt')
         chat_history_ids = model.generate(new_user_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
         answer = tokenizer.decode(chat_history_ids[:, new_user_input_ids.shape[-1]:][0], skip_special_tokens=True)
