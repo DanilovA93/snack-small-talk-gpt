32d0cb3a7e1125dc6e4034e17e613a231efbc6a9
DanilovA93 <danilovanton1993@mail.ru>
2024-04-12 00:02:37 +0300

    test

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 model_id = "mistralai/Mixtral-8x7B-Instruct-v0.1"
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 
-model = AutoModelForCausalLM.from_pretrained(model_id, device_map="auto")
+model = AutoModelForCausalLM.from_pretrained(model_id, load_in_4bit=True, device_map="auto")
 
 
 def generate(test_prompt) -> bytes:
