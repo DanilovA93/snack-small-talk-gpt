822e13cfffaef245de821f6353be3bd099e506ac
DanilovA93 <danilovanton1993@mail.ru>
2024-04-12 12:39:26 +0300

    test

--- server.py
+++ server.py
@@ -9,18 +9,20 @@ import torch
 model_id = "mistralai/Mixtral-8x7B-Instruct-v0.1"
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 
-bnb_config = BitsAndBytesConfig(
-    load_in_4bit=True,
-    bnb_4bit_use_double_quant=True,
-    bnb_4bit_quant_type="nf4",
-    bnb_4bit_compute_dtype=torch.float16
-)
+# bnb_config = BitsAndBytesConfig(
+#     load_in_4bit=True,
+#     bnb_4bit_use_double_quant=True,
+#     bnb_4bit_quant_type="nf4",
+#     bnb_4bit_compute_dtype=torch.float16
+# )
+#
+# model = AutoModelForCausalLM.from_pretrained(
+#     model_id,
+#     quantization_config=bnb_config,
+#     torch_dtype=torch.float16,
+# )
 
-model = AutoModelForCausalLM.from_pretrained(
-    model_id,
-    quantization_config=bnb_config,
-    torch_dtype=torch.float16,
-)
+model = AutoModelForCausalLM.from_pretrained(model_id, device_map="auto")
 
 def generate(test_prompt) -> str:
     messages = [
