62f6d8fcf68eb12dfa19788d46f125430e827d4a
DanilovA93 <danilovanton1993@mail.ru>
2024-04-17 19:53:40 +0300

    test

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/zsh
+sudo apt-get update
+sudo apt install python3-pip
+pip3 install transformers
+pip3 install torch
 sudo touch ./output.log
 sudo chmod 777 ./output.log
 sudo chmod 733 ./server.py
