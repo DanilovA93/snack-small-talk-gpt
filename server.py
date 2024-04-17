27b7fe971c07d2919679d150f1b286bab55a9097
DanilovA93 <danilovanton1993@mail.ru>
2024-04-17 22:41:37 +0300

    test

--- server.py
+++ server.py
@@ -9,17 +9,17 @@ import torch
 model_id = "mistralai/Mistral-7B-Instruct-v0.2"
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 
-# bnb_config = BitsAndBytesConfig(
-#     load_in_4bit=True,
-#     bnb_4bit_use_double_quant=True,
-#     bnb_4bit_quant_type="nf4",
-#     bnb_4bit_compute_dtype=torch.float16
-# )
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.float16
+)
 
 model = AutoModelForCausalLM.from_pretrained(
     model_id,
-    # quantization_config=bnb_config,
-    # torch_dtype=torch.float16,
+    quantization_config=bnb_config,
+    torch_dtype=torch.float16,
 )
 
 
