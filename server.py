fdbe43b7c9b53e37a2593f8983020c8dfb7d5832
DanilovA93 <danilovanton1993@mail.ru>
2024-04-19 02:37:09 +0300

    test

--- server.py
+++ server.py
@@ -16,6 +16,7 @@ def generate(test_prompt) -> str:
         'context': 'The option to convert models between FARM and transformers gives freedom to the user and let people easily switch between frameworks.'
     }
     res = nlp(QA_input)
+    print(res)
     return res
 
 
