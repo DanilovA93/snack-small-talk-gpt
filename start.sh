f4ec73458b08fd72a8df4b91406fd3e5061dcf8a
DanilovA93 <danilovanton1993@mail.ru>
2024-04-18 23:24:55 +0300

    test

--- start.sh
+++ start.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/zsh
 sudo apt-get update
 sudo apt install python3-pip
-pip3 install transformers
-pip3 install torch
-pip3 install bitsandbytes
-pip3 install accelerate
+pip3 install -r requirements.txt
 sudo touch ./output.log
 sudo chmod 777 ./output.log
 sudo chmod 733 ./server.py
