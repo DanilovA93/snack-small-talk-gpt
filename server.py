fdf27156de5d0cf342c3cfabc4859d79ba7682b6
DanilovA93 <danilovanton1993@mail.ru>
2024-04-19 00:32:58 +0300

    test

--- server.py
+++ server.py
@@ -36,6 +36,7 @@ def generate(test_prompt) -> str:
     outputs = model.generate(
         inputs,
         max_new_tokens=40,
+        do_sample=True,
         temperature=0.0
     )
     return tokenizer.decode(outputs[0], skip_special_tokens=True)
