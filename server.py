a8f455b1a9b7f1e0b7b4be83c77a37d13b7d4220
DanilovA93 <danilovanton1993@mail.ru>
2024-04-18 21:07:54 +0300

    test

--- server.py
+++ server.py
@@ -7,25 +7,16 @@ from llama_cpp import Llama
 
 # Set gpu_layers to the number of layers to offload to GPU. Set to 0 if no GPU acceleration is available on your system.
 llm = Llama(
-    model_path="./mistral-7b-instruct-v0.2.Q4_K_M.gguf",  # Download the model file first
+    model_path="./mistral-7b-instruct-v0.2.Q4_K_M.gguf",    # Download the model file first
     chat_format="llama-2",
-    n_ctx=32768,            # The max sequence length to use - note that longer sequence lengths require much more resources
-    n_threads=8,            # The number of CPU threads to use, tailor to your system and the resulting performance
-    n_gpu_layers=35,         # The number of layers to offload to GPU, if you have GPU acceleration available
-    max_tokens=32,
+    n_gpu_layers=35,        # The number of layers to offload to GPU, if you have GPU acceleration available
+    n_ctx=3584,             # The max sequence length to use - note that longer sequence lengths require much more resources
+    n_batch=521,
+    # n_threads=8,            # The number of CPU threads to use, tailor to your system and the resulting performance
+    verbose=True,
+    logits_all=True,
+    echo=True
 )
-
-
-# Simple inference example
-# output = llm(
-#     "<s>[INST] {prompt} [/INST]", # Prompt
-#     max_tokens=512,  # Generate up to 512 tokens
-#     stop=["</s>"],   # Example stop token - not necessarily correct for this specific model! Please check before using.
-#     echo=True        # Whether to echo the prompt
-# )
-
-
-# llm = Llama(model_path="./mistral-7b-instruct-v0.2.Q4_K_M.gguf", chat_format="llama-2")  # Set chat_format according to the model you are using
 
 
 def generate(test_prompt) -> str:
