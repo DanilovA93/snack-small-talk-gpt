b6a226e02722572185412e5f7e89df74a5325abf
DanilovA93 <danilovanton1993@mail.ru>
2024-04-18 19:21:37 +0300

    test

--- server.py
+++ server.py
@@ -20,7 +20,8 @@ bnb_config = BitsAndBytesConfig(
     load_in_4bit=True,
     bnb_4bit_use_double_quant=True,
     bnb_4bit_quant_type="nf4",
-    bnb_4bit_compute_dtype=torch.float16
+    bnb_4bit_compute_dtype=torch.float16,
+    disable_exllama=True
 )
 
 model = AutoModelForCausalLM.from_pretrained(
