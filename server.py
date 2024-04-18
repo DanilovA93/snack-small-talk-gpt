df0dc37030c8ea9336b5e1c7162b71a3baf5c407
DanilovA93 <danilovanton1993@mail.ru>
2024-04-19 00:14:26 +0300

    test

--- server.py
+++ server.py
@@ -9,20 +9,18 @@ model_id = "mistralai/Mistral-7B-Instruct-v0.2"
 access_token = "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"
 device = "cuda"
 
-bnb_config = BitsAndBytesConfig(
-    load_in_4bit=True,
-    bnb_4bit_use_double_quant=True,
-    bnb_4bit_quant_type="nf4",
-    bnb_4bit_compute_dtype=torch.float16,
-    disable_exllama=True
-)
+# bnb_config = BitsAndBytesConfig(
+#     load_in_4bit=True,
+#     bnb_4bit_use_double_quant=True,
+#     bnb_4bit_quant_type="nf4",
+#     bnb_4bit_compute_dtype=torch.float16,
+#     disable_exllama=True
+# )
 
 tokenizer = AutoTokenizer.from_pretrained(model_id, token=access_token)
 model = AutoModelForCausalLM.from_pretrained(
     model_id,
-    token=access_token,
-    quantization_config=bnb_config,
-    torch_dtype=torch.float16,
+    token=access_token
 )
 
 
