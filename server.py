56c8c66af6f2d109c9faa348728ec5efbd086501
DanilovA93 <danilovanton1993@mail.ru>
2024-04-21 00:10:30 +0300

    test

--- server.py
+++ server.py
@@ -103,14 +103,22 @@ def generate(
 
     print(inputs)
     print("-------------------------------------------------")
-    print(outputs)
+    print(outputs[0])
+    print(outputs[0][inputs.shape[1]:])
 
     answer = tokenizer.batch_decode(
         outputs[0][inputs.shape[1]:],
         skip_special_tokens=True
     )[0]
 
+    full_answer = tokenizer.batch_decode(
+        outputs,
+        skip_special_tokens=True
+    )[0]
+
     print(answer)
+    print("-------------------------------------------------")
+    print(full_answer)
 
     messages.append(
         {
