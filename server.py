b7cb1758c63d07975338be387a89d5202192d0e6
DanilovA93 <danilovanton1993@mail.ru>
2024-04-21 00:42:30 +0300

    test

--- server.py
+++ server.py
@@ -64,8 +64,8 @@ def generate(
             "content": prompt
         }
     ]
-    tokenized_chat = tokenizer.apply_chat_template(messages, return_tensors="pt")
-    inputs = tokenized_chat.to(device)
+    inputs = tokenizer.apply_chat_template(messages, return_tensors="pt")
+    # inputs = tokenized_chat.to(device)
 
     outputs = model.generate(
         inputs,
@@ -103,11 +103,10 @@ def generate(
     print("------------------------------------")
     print(outputs[0][inputs.shape[1]:])
 
-
-    # answer = tokenizer.decode(
-    #     outputs[0][inputs.shape[1]:],
-    #     skip_special_tokens=True
-    # )
+    answer = tokenizer.decode(
+        outputs[0][inputs.shape[1]:],
+        skip_special_tokens=True
+    )
     # answer = tokenizer.batch_decode(
     #     outputs[0][inputs.shape[1]:],
     #     skip_special_tokens=True
