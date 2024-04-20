b00a9d39f8f96cd86a948989798e65d47bb821da
DanilovA93 <danilovanton1993@mail.ru>
2024-04-20 22:43:50 +0300

    test

--- server.py
+++ server.py
@@ -93,18 +93,18 @@ def generate(
 
     decoded = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)
 
-    print("1. " + decoded)
+    print(decoded)
 
     answer = decoded[0]
 
-    messages.append(
-        {
-            "role": "assistant",
-            "content": answer
-        }
-    )
+    # messages.append(
+    #     {
+    #         "role": "assistant",
+    #         "content": answer
+    #     }
+    # )
 
-    return answer
+    return "answer"
 
 
 class Handler(http.server.SimpleHTTPRequestHandler):
