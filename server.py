0b3a63093db077ff11d0f85e46b0e945a0b2b3fa
DanilovA93 <danilovanton1993@mail.ru>
2024-04-21 02:04:21 +0300

    test

--- server.py
+++ server.py
@@ -137,18 +137,15 @@ class Handler(http.server.SimpleHTTPRequestHandler):
 
         self._set_headers()
 
-        try:
-            answer = process(
-                rq_body['username'],
-                rq_body['prompt'],
-                rq_body.get("max_new_tokens", None),
-                rq_body.get("temperature", None),
-                rq_body.get("top_p", None),
-                rq_body.get("top_k", None)
-            )
-            self.wfile.write(answer.encode())
-        except Exception as err:
-            self.wfile.write(f"Ошибка: {err}".encode())
+        answer = process(
+            rq_body['username'],
+            rq_body['prompt'],
+            rq_body.get("max_new_tokens", None),
+            rq_body.get("temperature", None),
+            rq_body.get("top_p", None),
+            rq_body.get("top_k", None)
+        )
+        self.wfile.write(answer.encode())
 
 
     def do_GET(self):
