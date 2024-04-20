3b137da29a2c80cabfe36730fbcdf43dd301a5c4
DanilovA93 <danilovanton1993@mail.ru>
2024-04-21 00:28:09 +0300

    test

--- server.py
+++ server.py
@@ -97,7 +97,10 @@ def generate(
         #       integer or null
         top_k=top_k
     )
-    answer = tokenizer.batch_decode(outputs, skip_special_tokens=True)
+    answer = tokenizer.batch_decode(
+        outputs[0],
+        skip_special_tokens=True
+    )
     # answer = tokenizer.batch_decode(
     #     outputs[0][inputs.shape[1]:],
     #     skip_special_tokens=True
