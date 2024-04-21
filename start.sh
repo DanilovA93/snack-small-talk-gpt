57cff917e7046484da6012e2f301e464a9cc677e
DanilovA93 <danilovanton1993@mail.ru>
2024-04-21 15:41:43 +0300

    test

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 sudo apt-get update
 sudo apt install python3-pip
 pip3 install -r requirements.txt
-sudo touch ./output.log
-sudo chmod 777 ./output.log
-sudo chmod 733 ./server.py
+touch ./output.log
+chmod 777 ./output.log
+chmod 733 ./server.py
 nohup python3 ./server.py > ./output.log &
