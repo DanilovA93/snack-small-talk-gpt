09e4bacaae750556bfd7d8dd83e57b2a516af762
DanilovA93 <danilovanton1993@mail.ru>
2024-04-25 02:09:30 +0300

    ref

--- GPTService.py
+++ GPTService.py
@@ -28,40 +28,24 @@ def process(
         top_k=40,
 ) -> str:
 
+    generate_kwargs = dict(
+        temperature=temperature,
+        max_new_tokens=max_new_tokens,
+        top_p=top_p,
+        repetition_penalty=1.2,
+        do_sample=True,
+        seed=42,
+    )
+
     tokenized_chat = tokenizer.apply_chat_template(chat, return_tensors="pt")
     inputs = tokenized_chat.to(device)
 
     outputs = model.generate(
         inputs,
-
-        #       integer or null >= 0
-        #       Default: null
-        #       The maximum number of tokens to generate in the completion.
-        #
-        #       The token count of your prompt plus max_new_tokens cannot exceed the model's context length.
-        max_new_tokens=max_new_tokens,
-
-        #       bool
-        do_sample=True,
-
-        #       number or null [ 0 .. 1 ]
-        #       Default: 0.7
-        #       What sampling temperature to use, between 0.0 and 1.0. Higher values like 0.8 will make the output more random, while lower values like 0.2 will make it more focused and deterministic.
-        #
-        #       We generally recommend altering this or top_p but not both.
-        temperature=temperature,
-
-        #       number or null [ 0 .. 1 ]
-        #       Default: 1
-        #       Nucleus sampling, where the model considers the results of the tokens with top_p probability mass. So 0.1 means only the tokens comprising the top 10% probability mass are considered.
-        #
-        #       We generally recommend altering this or temperature but not both.
-        top_p=top_p,
-
-        #       number or null [ 0 .. 200 ]
-        #       Default: 50
-        #       Controls the number of most-likely candidates that the model considers for the next token.
-        top_k=top_k
+        **generate_kwargs,
+        stream=False,
+        details=True,
+        return_full_text=False
     )
 
     gen_answer = tokenizer.batch_decode(
