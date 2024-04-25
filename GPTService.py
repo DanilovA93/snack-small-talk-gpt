a15b1fd40991258089949d9f4ef1dac677b2f12e
DanilovA93 <danilovanton1993@mail.ru>
2024-04-26 01:45:14 +0300

    ref

--- GPTService.py
+++ GPTService.py
@@ -3,20 +3,25 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 
 torch.random.manual_seed(0)
 
+print("model...")
 model = AutoModelForCausalLM.from_pretrained(
     "microsoft/Phi-3-mini-128k-instruct",
     device_map="cuda",
     torch_dtype="auto",
     trust_remote_code=True,
 )
+
+print("tokenizer...")
 tokenizer = AutoTokenizer.from_pretrained("microsoft/Phi-3-mini-128k-instruct")
 
+print("pipeline...")
 pipe = pipeline(
     "text-generation",
     model=model,
     tokenizer=tokenizer,
 )
 
+print("generation_args...")
 generation_args = {
     "max_new_tokens": 100,
     "return_full_text": False,
@@ -24,6 +29,7 @@ generation_args = {
     "do_sample": False,
 }
 
+print("ready")
 def process(chat) -> str:
     output = pipe(chat, **generation_args)
     print(output[0]['generated_text'])
