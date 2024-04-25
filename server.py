d699ea56022a5a124fc903b8fbe3cf30daffc332
DanilovA93 <danilovanton1993@mail.ru>
2024-04-25 19:17:01 +0300

    ref

--- server.py
+++ server.py
@@ -146,15 +146,17 @@ class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        message =  threading.currentThread().getName()
+        message = threading.currentThread().getName()
         self.wfile.write(message)
         self.wfile.write('\n')
         return
 
+
 class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
     """Handle requests in a separate thread."""
 
+
 if __name__ == '__main__':
-    server = ThreadedHTTPServer(('', 8080), Handler)
-    print ('Starting server, use <Ctrl-C> to stop')
+    server = ThreadedHTTPServer(('', 8001), Handler)
+    print('Starting server, use <Ctrl-C> to stop')
     server.serve_forever()
