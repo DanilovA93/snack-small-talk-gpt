340df1f7c54dc8c44b8f9f16993be7b3195eccfc
DanilovA93 <danilovanton1993@mail.ru>
2024-04-27 23:54:09 +0300

    ref

--- GPTService.py
+++ GPTService.py
@@ -4,7 +4,7 @@ from llama_cpp import Llama
 llm = Llama(
     model_path="./Phi-3-mini-4k-instruct-q4.gguf",  # path to GGUF file
     chat_format="llama-2",
-    n_ctx=2048,  # The max sequence length to use - note that longer sequence lengths require much more resources
+    n_ctx=256,  # The max sequence length to use - note that longer sequence lengths require much more resources
     n_threads=8, # The number of CPU threads to use, tailor to your system and the resulting performance
     n_gpu_layers=-1, # The number of layers to offload to GPU, if you have GPU acceleration available. Set to 0 if no GPU acceleration is available on your system.
     seed=1337
