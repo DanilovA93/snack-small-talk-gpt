ffdbd6d09f4449335c5c632521e3f7299d8df849
DanilovA93 <danilovanton1993@mail.ru>
2024-05-28 00:04:31 +0300

    mistral

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/zsh
 sudo apt-get update
-#sudo apt install python3-pip
-pip3 install -r requirements.txt
+sudo apt install python3-pip
+pip3 install -r --break-system-packages requirements.txt
 touch ./output.log
 chmod 777 ./output.log
 chmod 733 ./server.py
